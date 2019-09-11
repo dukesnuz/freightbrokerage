f7c268129d2d5dc79ba4d2204fd2ecc4905dfd2e
David Petringa <david@ajaxtransport.com>
2019-09-11 09:21:51 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Freight Brokerage</title>
+
+		<meta charset="UTF-8">
+		<meta name="description" content="Freight brokers moving truckload and less than truckload freight. Great freight rates for carriers and shippers.">
+		<meta name="keywords" content="post loads online, search for loads, trucking, freight logistics, trucking companies, freight, brokers, logistics, shippers, carriers, ajax transport">
+		<meta name="author" content="David Petringa, Coded April 2018">
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+		<link href = "http://www.dukesnuz.com/css_libs/dukes_normalize.css" rel = "stylesheet">
+		<link href="styles.css?m=<?php echo rand(); ?>" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+
+		<header>
+			<h1>Freight Brokerage</h1>
+			<h2>Ajax, A Freight Broker</h2>
+		</header>
+
+		<section class="content">
+			<h3>We Move Freight</h3>
+			<div class="register">
+				<p>
+					<a href="http://www.ajaxtransport.com/carrier/register" target="blank">Carriers Register Now</a>
+				</p>
+			</div>
+			<div class="register">
+				<p>
+					<a href="http://ajaxtransport.com/shipper/request_rate.php" target="blank">Shippers Get a Rate</a>
+				</p>
+			</div>
+			<ul>
+				<li>
+					Competative Rates
+				</li>
+				<li>
+					Established in 1955
+				</li>
+				<li>
+					Quality Service
+				</li>
+				<li>
+					Premium Customer Service
+				</li>
+				<li>
+					Exceptional Carrier Support
+				</li>
+				<li>
+					Financial Stable
+				</li>
+				<li>
+					Online, Anytime, Anywhere
+				</li>
+			</ul>
+		</section>
+
+		<footer>
+			<h5>Freightbrokerage.co is part of Ajax Trans. Co., Inc. A freight brokerage company.</h5>
+			<ul>
+				<li>
+					<a href="http://www.ajaxtransport.com" target="blank">Online freight brokerage at Ajax Transport</a>
+				</li>
+				<li>
+					<a href="http://www.thecentraldispatch.com" target="blank">Valuable transportation links at The Central Dispatch</a>
+				</li>
+				<li>
+					<a href="http://www.loadedandrolling.co" target="blank">Carrier look up at Loaded and Rolling .co</a>
+				</li>
+				<li>
+					<a href="http://www.loadedandrolling.com" target="blank">Load posting board Loaded and Rolling .com</a>
+				</li>
+				<li>
+					<a href="http://www.ajaxforums.com" target="blank">Ajax Forums</a>
+				</li>
+			</ul>
+			<p>
+				Copyright &copy; <?php echo date("Y"); ?>
+				freightbrokerage.co. All rights reserved.
+			</p>
+			<p>
+				Designed and developed by: <a href="http://www.dukesnuz.com" target="blank">David Petringa</a>
+			</p>
+		</footer>
+		<!-- Default Statcounter code for Freightbrokerage.co
+		http://www.freightbrokerage.co -->
+		<script type="text/javascript">
+			var sc_project = 6991393;
+			var sc_invisible = 1;
+			var sc_security = "18c14c63";
+			var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www.");
+			document.write("<sc" + "ript type='text/javascript' src='" + scJsHost + "statcounter.com/counter/counter.js'></" + "script>");
+		</script>
+		<noscript>
+			<div class="statcounter">
+				<a title="Web Analytics
+				Made Easy - StatCounter" href="http://statcounter.com/"
+				target="_blank"><img class="statcounter"
+				src="//c.statcounter.com/6991393/0/18c14c63/0/" alt="Web
+				Analytics Made Easy - StatCounter"></a>
+			</div>
+		</noscript>
+		<!-- End of Statcounter Code -->
+	</body>
+</html>
